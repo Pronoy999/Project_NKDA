4f70a2309dbdff60040ad89642c29c833f62c0ba
Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>
2020-02-24 21:07:10 +0530

    Added Get Toilet Master and details.

--- DB_Scripts/Stored_Procedures/sp_GetToiletDetails.sql
+++ DB_Scripts/Stored_Procedures/sp_GetToiletDetails.sql
@@ -1,11 +1,19 @@
-DROP PROCEDURE IF EXISTS sp_GetToiletMasters;
-CREATE PROCEDURE sp_GetToiletMasters()
+DROP PROCEDURE IF EXISTS sp_GetToiletDetails;
+CREATE PROCEDURE sp_GetToiletDetails(IN toiletID INT)
 BEGIN
-    SELECT tm.id,
-           tm.toilet_name,
-           tm.address,
-           tm.total_seats,
-           tm.technician_id
-    FROM toilet_master tm;
+    IF toiletID > 0 THEN
+        SELECT tm.id,
+               tm.toilet_name,
+               tm.address,
+               tm.total_seats,
+               tm.technician_id,
+               td.id,
+               td.seat_number
+        FROM toilet_master tm
+                 INNER JOIN toilet_details td
+                            ON td.toilet_id = tm.id
+        WHERE tm.id = toiletID;
+    ELSE
+        SELECT -1;
+    END IF;
 end;
-
